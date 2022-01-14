34e05988148c915fe05fbedbcc3cc43eb7ae4464
AMIR HOSSAM <amir_h56395@cic-cairo.com>
2022-01-14 02:00:08 +0200

    DOR and DOD

---
+++ DOR  and DOR.py
@@ -0,0 +1,11 @@
+'''
+Definition of Ready
+
+The definition of ready defines the quality criteria for the creation of any User story and the product itself
+
+
+Definition of done
+
+The definition of done establishes the quality criteria of the delivery of product increament and the work completed.
+
+'''
